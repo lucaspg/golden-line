31149ce8c80b7190709f0554304f2d65675db4ab
Lucas Gonçalves <l.paulon97@gmail.com>
2018-09-25 23:57:44 -0300

    Add enzyme

---
+++ src/components/App/tests/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from '../../../enzyme';
+import App from '../index';
+import AppBar from '../../AppBar';
+
+describe('<App />', () => {
+  it('renders the app bar and the restroom list', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(AppBar)).toHaveLength(1);
+  });
+});
